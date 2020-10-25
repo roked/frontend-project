24304577bcb67e093f121abbba14c9bdf78bb72e
Mitko Donchev <donchevm@coventry.ac.uk>
2020-10-25 16:49:29 +0000

    This feature (a grid/deck of properties) is completed. I used CardDeck and card from the react-boostrap docs and this way I can list all properties in a nicely and easy to understant way. Later on I will get all properties from the DB and load them using a loop.

--- src/components/home.js
+++ src/components/home.js
@@ -1,9 +1,51 @@
-import React from 'react';
+import React     from 'react';
+import Card      from 'react-bootstrap/Card'
+import CardDeck from 'react-bootstrap/CardDeck'
 
 //define the home page component
+//add card deck with cards
+//TODO - use a loop and show all properties 
 const Home = () => (
-    <div>
-        This is the home page!
+    <div className="container">
+        <h1>Our active listings!</h1>
+        <CardDeck>
+          <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+              <Card.Title>Property</Card.Title>
+              <Card.Text>
+                Here will be the property description.
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+          </Card>
+          <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+              <Card.Title>Property</Card.Title>
+              <Card.Text>
+                Here will be the property description.
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+          </Card>
+          <Card>
+            <Card.Img variant="top" src="holder.js/100px160" />
+            <Card.Body>
+              <Card.Title>Property</Card.Title>
+              <Card.Text>
+                Here will be the property description.
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+          </Card>
+        </CardDeck>
     </div>
 );
 
